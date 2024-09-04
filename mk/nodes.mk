78e5b2d33a2cd0c8553d795e47526436ddf73197
sisaacs <steve.isaacs@embeddeddomain.com>
2024-09-04 05:38:31 -0700

    Changed node declaration to not signal an error but instead issue an Attention of a node has already been declared.

--- mk/nodes.mk
+++ mk/nodes.mk
@@ -267,7 +267,7 @@ $(call Add-Help,${_macro})
 define ${_macro}
   $(call Enter-Macro,$(0),node=$(1) path=$(2))
   $(if $(call node-is-declared($(1))),
-    $(call Signal-Error,Node $(1) has already been declared.)
+    $(call Attention,Node $(1) has already been declared.)
   ,
     $(if $(2),
       $(call Info,Declaring root node:$(1))
@@ -330,7 +330,7 @@ $(call Add-Help,${_macro})
 define ${_macro}
   $(call Enter-Macro,$(0),node=$(1) parent=$(2) dir=$(3))
   $(if $(call node-is-declared,$(1)),
-    $(call Signal-Error,Node $(1) has already been declared.)
+    $(call Attention,Node $(1) has already been declared.)
   ,
     $(if $(2),
       $(if $(call node-is-declared,$(2)),
